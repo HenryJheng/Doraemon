c9a9a8b536ca6868695b932ae319d037c042fe0f
Henry Jheng <henry021217@gmail.com>
2022-09-03 14:25:03 +0800

    Can change Doraemon color.

---
+++ app.js
@@ -0,0 +1,13 @@
+function blue() {
+    document.querySelector('section.head_blue').style.backgroundColor = "#0289ff";
+     };
+function red() {
+    document.querySelector('section.head_blue').style.backgroundColor = "red";
+     };
+function yellow() {
+    document.querySelector('section.head_blue').style.backgroundColor = "yellow";
+     };
+function green() {
+    document.querySelector('section.head_blue').style.backgroundColor = "green";
+     };     
+
